6c90c669a1dd8a3c8317baf7bea3080b19b6f9d6
Kirill Fedyanin <kirill.fedyanin@gmail.com>
2022-09-23 17:28:34 +0400

    Check ln model

--- explore/benchmark.py
+++ explore/benchmark.py
@@ -1,7 +1,6 @@
 """
-Lvl 1
-Just bench
 """
+
 import torch
 import numpy as np
 import pandas as pd
@@ -20,8 +19,11 @@ from sensorium.utility.measure_helpers import get_df_for_scores
 basepath = "./notebooks/data/"
 
 
-def init_loaders():
-    filenames = [os.path.join(basepath, file) for file in os.listdir(basepath) if ".zip" in file ]
+def init_loaders(single=False):
+    if single:
+        filenames = ['notebooks/data/static26872-17-20-GrayImageNet-94c6ff995dac583098847cfecd43e7b6.zip', ]
+    else:
+        filenames = [os.path.join(basepath, file) for file in os.listdir(basepath) if ".zip" in file ]
 
     dataset_fn = 'sensorium.datasets.static_loaders'
     dataset_config = {
@@ -37,11 +39,10 @@ def init_loaders():
     return dataloaders
 
 
-def benchmark(dataloaders, model):
+def benchmark(dataloaders, model, tier='validation'):
     correlation_to_average = get_signal_correlations(
-        model, dataloaders, tier="test", device="cuda", as_dict=True
+        model, dataloaders, tier=tier, device="cuda", as_dict=True
     )
-    print(correlation_to_average['23964-4-22'].shape)
 
     measure_attribute = "Correlation to Average"
     df = get_df_for_scores(
@@ -105,12 +106,53 @@ def init_sota(dataloaders, checkpoint_path):
     return model
 
 
+def ln_model(dataloaders, checkpoint_path):
+    model_fn = 'sensorium.models.stacked_core_full_gauss_readout'
+    model_config = {'pad_input': False,
+                    'stack': -1,
+                    'layers': 3,
+                    'input_kern': 9,
+                    'gamma_input': 6.3831,
+                    'gamma_readout': 0.0076,
+                    'hidden_kern': 7,
+                    'hidden_channels': 64,
+                    'grid_mean_predictor': {
+                        'type': 'cortex',
+                        'input_dimensions': 2,
+                        'hidden_layers': 1,
+                        'hidden_features': 30,
+                        'final_tanh': True
+                    },
+                    'depth_separable': True,
+                    'init_sigma': 0.1,
+                    'init_mu_range': 0.3,
+                    'gauss_type': 'full',
+                    'linear': True
+                    }
+    model = get_model(model_fn=model_fn,
+                      model_config=model_config,
+                      dataloaders=dataloaders,
+                      seed=42, )
+    model.load_state_dict(torch.load(checkpoint_path))
+    model.eval()
+    return model
+
+
 def main():
+    # just change the model here
     dataloaders = init_loaders()
-    model = init_sota(dataloaders, "model_checkpoints/pretrained/generalization_model.pth")
-    benchmark(dataloaders, model)
+    model = init_sota(
+        dataloaders,
+        "notebooks/model_tutorial/model_checkpoints/pretrained/generalization_model.pth"
+    )
 
+    # dataloaders = init_loaders(single=True)
+    # model = ln_model(
+    #     dataloaders,
+    #     "model_checkpoints/notebook_examples/sensorium_ln_model.pth"
+    # )
 
+    benchmark(dataloaders, model, tier='test')
 
 
 if __name__ == '__main__':
